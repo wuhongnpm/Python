9a511b79911858854d8c7c432c0c12c60ed2146f
SoapEye <lious.q.k@gmail.com>
2021-06-10 17:35:09 +0800

    2
    1

--- auto/cloud/demo.py
+++ auto/cloud/demo.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-dr = webdriver.Chrome(executable_path="D:\package\chromedriver\chromedriver.exe")
+dr = webdriver.Chrome(executable_path="D:\install\chromedriver\chromedriver.exe")
 
 dr.get('http://cloud.test.cclyun.cn/')
 print(dr.title)
